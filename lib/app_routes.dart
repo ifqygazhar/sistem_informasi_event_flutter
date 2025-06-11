88cb483f4f922ddf09cf507199edbb7de70875b5
ifqygazhar <ifqygazhar@gmail.com>
2025-06-11 18:24:55 +0700

    Add EventDetailPage and update routing in AppRoutes

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:get/route_manager.dart';
+import 'package:sistem_informasi/pages/event/event_detail.dart';
 import 'package:sistem_informasi/pages/home/binding/home_binding.dart';
 import 'package:sistem_informasi/pages/home/home.dart';
 import 'package:sistem_informasi/pages/splash/splash.dart';
@@ -8,13 +9,27 @@ class AppRoutes {
   static const String home = '/home';
   static const String login = '/login';
   static const String register = '/register';
-  static const String eventDetail = '/event-detail';
+  static const String eventDetail = '/event/:id';
   static const String createEvent = '/create-event';
   static const String editEvent = '/edit-event';
 
   static final routes = [
-    GetPage(name: splash, page: () => const SplashPage()),
-    GetPage(name: home, page: () => const HomePage(), binding: HomeBinding()),
+    GetPage(
+      name: splash,
+      page: () => const SplashPage(),
+      transition: Transition.leftToRight,
+    ),
+    GetPage(
+      name: home,
+      page: () => const HomePage(),
+      binding: HomeBinding(),
+      transition: Transition.leftToRight,
+    ),
+    GetPage(
+      name: eventDetail,
+      page: () => const EventDetailPage(),
+      transition: Transition.leftToRight,
+    ),
     // GetPage(name: eventDetail, page: () => const EventDetailPage()),
     // GetPage(name: createEvent, page: () => const CreateEventPage()),
     // GetPage(name: editEvent, page: () => const EditEventPage()),
--- lib/pages/home/widgets/event.dart
+++ lib/pages/home/widgets/event.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sistem_informasi/app_routes.dart';
 import 'package:sistem_informasi/models/event.dart';
 import 'package:sistem_informasi/utils/colors.dart';
 import 'package:sistem_informasi/utils/text.dart';
@@ -17,135 +19,138 @@ class EventWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8),
-      padding: const EdgeInsets.only(bottom: 8),
-      decoration: BoxDecoration(
-        color: primaryColor,
-        borderRadius: BorderRadius.circular(sizeHeight * 0.02),
-      ),
-      child: Column(
-        spacing: sizeHeight * 0.006,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(sizeHeight * 0.02),
-              topRight: Radius.circular(sizeHeight * 0.02),
+    return GestureDetector(
+      onTap: () {
+        Get.toNamed(AppRoutes.eventDetail, arguments: event);
+      },
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        padding: const EdgeInsets.only(bottom: 8),
+        decoration: BoxDecoration(
+          color: primaryColor,
+          borderRadius: BorderRadius.circular(sizeHeight * 0.02),
+        ),
+        child: Column(
+          spacing: sizeHeight * 0.006,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(sizeHeight * 0.02),
+                topRight: Radius.circular(sizeHeight * 0.02),
+              ),
+              child: CachedNetworkImage(
+                fit: BoxFit.cover,
+                height: sizeHeight * 0.2,
+                width: double.infinity,
+                imageUrl:
+                    event.image ??
+                    "https://images.pexels.com/photos/1324803/pexels-photo-1324803.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+                placeholder:
+                    (context, url) => Container(
+                      height: sizeHeight * 0.2,
+                      color: Colors.grey[300],
+                      child: const Center(child: CircularProgressIndicator()),
+                    ),
+                errorWidget:
+                    (context, url, error) => Container(
+                      height: sizeHeight * 0.2,
+                      color: Colors.grey[300],
+                      child: const Icon(Icons.error),
+                    ),
+              ),
             ),
-            child: CachedNetworkImage(
-              fit: BoxFit.cover,
-              height: sizeHeight * 0.2,
-              width: double.infinity,
-              imageUrl:
-                  event.image ??
-                  "https://images.pexels.com/photos/1324803/pexels-photo-1324803.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-              placeholder:
-                  (context, url) => Container(
-                    height: sizeHeight * 0.2,
-                    color: Colors.grey[300],
-                    child: const Center(child: CircularProgressIndicator()),
-                  ),
-              errorWidget:
-                  (context, url, error) => Container(
-                    height: sizeHeight * 0.2,
-                    color: Colors.grey[300],
-                    child: const Icon(Icons.error),
-                  ),
-            ),
-          ),
 
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: sizeWidth * 0.03),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextGlobalWidget(
-                    text: event.title,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: sizeWidth * 0.03),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextGlobalWidget(
+                      text: event.title,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      textOverflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Icon(Icons.group_add_outlined, size: 15, color: Colors.white),
+                  TextGlobalWidget(
+                    text:
+                        " ${event.participantsCount}/${event.capacity ?? '∞'}",
                     fontSize: 15,
                     fontWeight: FontWeight.bold,
                   ),
-                ),
-                const SizedBox(width: 8),
-                Icon(Icons.group_add_outlined, size: 15, color: Colors.white),
-                TextGlobalWidget(
-                  text: " ${event.participantsCount}/${event.capacity ?? '∞'}",
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
 
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: sizeWidth * 0.03),
-            child: Row(
-              children: [
-                const Icon(Icons.person, color: Colors.white, size: 16),
-                SizedBox(width: sizeWidth * 0.01),
-                Expanded(
-                  child: TextGlobalWidget(
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: sizeWidth * 0.03),
+              child: Row(
+                children: [
+                  const Icon(Icons.person, color: Colors.white, size: 16),
+                  SizedBox(width: sizeWidth * 0.01),
+                  TextGlobalWidget(
                     text: event.creator?.name ?? "Unknown",
                     fontSize: 14,
                     fontWeight: FontWeight.w500,
+                    textOverflow: TextOverflow.ellipsis,
+                    maxLines: 1,
                   ),
-                ),
-              ],
-            ),
-          ),
-
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: sizeWidth * 0.03),
-            child: Row(
-              children: [
-                const Icon(Icons.location_on, color: Colors.white, size: 16),
-                SizedBox(width: sizeWidth * 0.01),
-                Expanded(
-                  child: TextGlobalWidget(
+                  SizedBox(width: sizeWidth * 0.01),
+                  const Icon(Icons.location_on, color: Colors.white, size: 16),
+                  SizedBox(width: sizeWidth * 0.01),
+                  TextGlobalWidget(
                     text: event.location,
                     fontSize: 14,
                     fontWeight: FontWeight.w500,
+                    textOverflow: TextOverflow.ellipsis,
+                    maxLines: 1,
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
 
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: sizeWidth * 0.03),
-            child: TextGlobalWidget(
-              text: event.description,
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-              maxLines: 2,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: sizeWidth * 0.03),
+              child: TextGlobalWidget(
+                text: event.description,
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                textOverflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
             ),
-          ),
 
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: sizeWidth * 0.03),
-            child: GestureDetector(
-              onTap: () {
-                // Handle register tap
-              },
-              child: Container(
-                height: sizeHeight * 0.04,
-                width: double.infinity,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: event.hasReachedCapacity ? Colors.grey : Colors.white,
-                  borderRadius: BorderRadius.circular(sizeHeight * 0.008),
-                ),
-                child: TextGlobalWidget(
-                  text: event.hasReachedCapacity ? "Full" : "Register",
-                  fontSize: 14,
-                  fontColor:
-                      event.hasReachedCapacity ? Colors.white : primaryColor,
-                  fontWeight: FontWeight.bold,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: sizeWidth * 0.03),
+              child: GestureDetector(
+                onTap: () {
+                  // Handle register tap
+                },
+                child: Container(
+                  height: sizeHeight * 0.04,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color:
+                        event.hasReachedCapacity ? Colors.grey : Colors.white,
+                    borderRadius: BorderRadius.circular(sizeHeight * 0.008),
+                  ),
+                  child: TextGlobalWidget(
+                    text: event.hasReachedCapacity ? "Full" : "Register",
+                    fontSize: 14,
+                    fontColor:
+                        event.hasReachedCapacity ? Colors.white : primaryColor,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
